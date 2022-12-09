5a783a0fdc8feb2582f881bf67207fd4cb16b8d8
zxczxc123 <108798401+kingofaussie@users.noreply.github.com>
2022-12-09 10:15:49 +0900

    mis

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { useState } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import TodoList from './components/TodoList/TodoList';
-import { DarkModeProvider } from './context/DarkModeContext';
+import  TodoList from './components/TodoList/TodoList';
 
-const filters = ['all', 'active', 'completed'];
 function App() {
-  const [filter, setFilter] = useState(filters[0]);
   return (
-    <DarkModeProvider>
-      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
-      <TodoList filter={filter} />
-    </DarkModeProvider>
+  <div>
+    <TodoList />
+  </div>
   );
 }
+  
+
 
 export default App;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,48 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import AddTodo from '../AddtTodo/AddTodo';
-import Todo from '../Todo/Todo';
-import styles from './TodoList.module.css';
+import React from 'react';
+import { useState } from 'react';
 
-export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
-
-  const handleAdd = (todo) => setTodos([...todos, todo]);
-  const handleUpdate = (updated) =>
-    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
-  const handleDelete = (deleted) =>
-    setTodos(todos.filter((t) => t.id !== deleted.id));
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
-  const filtered = getFilteredItems(todos, filter);
+export default function TodoList() {
+    const [todos, setTodos] = useState([
+      {id: '123', text: '장보기', status: 'active'},
+      {id: '124', text: '공부보기', status: 'active'},
+    ]); // 해야할 목록 리스트
   return (
-    <section className={styles.container}>
-      <ul className={styles.list}>
-        {filtered.map((item) => (
-          <Todo
-            key={item.id}
-            todo={item}
-            onUpdate={handleUpdate}
-            onDelete={handleDelete}
-          />
-        ))}
+    <section>
+      <ul>
+          {
+            todos.map(item => <li key={item.id}>{item.text}</li>)
+          }
       </ul>
-      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
 
-function readTodosFromLocalStorage() {
-  console.log('readTodosFromLocalStorage');
-  const todos = localStorage.getItem('todos');
-  return todos ? JSON.parse(todos) : [];
-}
-
-function getFilteredItems(todos, filter) {
-  if (filter === 'all') {
-    return todos;
-  }
-  return todos.filter((todo) => todo.status === filter);
-}
