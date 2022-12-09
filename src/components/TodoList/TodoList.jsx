ef4e1c7975ab962bf6030db109c53942e5895c1d
zxczxc123 <108798401+kingofaussie@users.noreply.github.com>
2022-12-09 10:46:04 +0900

    make List and add btn

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { useState } from 'react';
+import AddTodo from '../AddTodo/AddTodo';
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
       {id: '123', text: '장보기', status: 'active'},
       {id: '124', text: '공부보기', status: 'active'},
     ]); // 해야할 목록 리스트
+    const handleAdd = (todo) => setTodos([...todos , todo]);
+      // 새로운 투두를 todos에 업데이트 해야함
+  
   return (
     <section>
       <ul>
-          {
-            todos.map(item => <li key={item.id}>{item.text}</li>)
-          }
+          {todos.map((item) => (
+          <li key={item.id}>{item.text}</li>
+          ))}
       </ul>
+      <AddTodo onAdd={handleAdd}/>
     </section>
+    
   );
 }
 
